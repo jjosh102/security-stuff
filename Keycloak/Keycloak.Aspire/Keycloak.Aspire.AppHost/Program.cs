a0d86f74c4ff29e76d16d6e122f725321ef32ca4
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2025-07-24 19:29:39 +0800

    Add Keycloak.Api

--- Keycloak/Keycloak.Aspire/Keycloak.Aspire.AppHost/Program.cs
+++ Keycloak/Keycloak.Aspire/Keycloak.Aspire.AppHost/Program.cs
@@ -2,4 +2,9 @@ var builder = DistributedApplication.CreateBuilder(args);
 var keycloak = builder.AddKeycloak("keycloak", 8080)
                       .WithDataVolume()
                       .WithExternalHttpEndpoints();
+
+var api = builder.AddProject<Projects.Keycloak_Api>("api")
+                 .WithExternalHttpEndpoints()
+                 .WithReference(keycloak);
+
 builder.Build().Run();
